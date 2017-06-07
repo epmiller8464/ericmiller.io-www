2b82792543dec6370577cf253a0246bf931517ca
Eric Miller <epmiller8464@gmail.com>
2017-06-07 01:24:04 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var sassMiddleware = require('node-sass-middleware')
+let hbs = require('express-handlebars')
+var index = require('./routes/index')
+var users = require('./routes/users')
+
+var app = express()
+
+// view engine setup
+var exphbs = hbs.create({
+  extname: 'hbs', defaultLayout: 'layout', helpers: {
+    section: function (name, options) {
+      if(!this._sections) {
+        this._sections = {}
+      }
+      this._sections[name] = options.fn(this)
+      return null
+    },
+    increment: function (index) {
+      return index + 1
+    },
+    formatDate: function (date) {
+      return moment(date).format('L')
+
+    },
+    formatDateTime: function (date) {
+      return moment(date).format('LLL')
+
+    },
+    printCode: function (code) {
+      return JSON.stringify(code, null, 2)
+    },
+    ifCond: function (v1, operator, v2, options) {
+
+      switch (operator) {
+        case '==':
+          return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+          return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+          return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+          return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+          return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+          return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+          return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+          return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+          return options.inverse(this);
+      }
+    },
+    printYesOrNo: function (isTrue) {
+      return (isTrue) ? 'Yes' : 'No'
+    }
+  }
+})
+// view engine setup
+app.engine('hbs', exphbs.engine);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', index)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = app
