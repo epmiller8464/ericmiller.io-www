e73c4ea3d59b7228ee823fc5d7428b9ad4d2447c
Eric Miller <epmiller8464@gmail.com>
2017-06-08 00:18:07 -0500

    rerecord fix attempt

--- routes/index.js
+++ routes/index.js
@@ -16,34 +16,20 @@ router.use('/uploads', (req, response, next) => {
   var uri = url.parse(req.url).pathname,
     filename = path.join(process.cwd()+'/uploads', uri)
   // var filename = `./../uploads${req.url}`
-  fs.exists(filename, function (exists) {
-    if (!exists) {
-      response.writeHead(404, {
+  fs.readFile(filename, 'binary', function (err, file) {
+    if (err) {
+      response.writeHead(500, {
         'Content-Type': 'text/plain'
       })
-      response.write('404 Not Found: ' + filename + '\n')
+      response.write(err + '\n')
       response.end()
       return
     }
 
-    if (fs.statSync(filename).isDirectory()) {
-      filename += '/index.html'
-    }
-
-    fs.readFile(filename, 'binary', function (err, file) {
-      if (err) {
-        response.writeHead(500, {
-          'Content-Type': 'text/plain'
-        })
-        response.write(err + '\n')
-        response.end()
-        return
-      }
-
-      response.writeHead(200)
-      response.write(file, 'binary')
-      response.end()
-    })
+    response.writeHead(200)
+    response.write(file, 'binary')
+    response.end()
   })
+
 })
 module.exports = router
