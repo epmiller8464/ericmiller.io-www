5a997d061293b7ae61328781db5141da207e8552
Eric Miller <epmiller8464@gmail.com>
2017-07-19 18:38:04 -0500

    aws s3 service integration

--- dist/bin/www.js
+++ dist/bin/www.js
@@ -3,7 +3,7 @@
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 if (process.env.NODE_ENV === 'development') {}
-require('dotenv').config();
+// require('dotenv').config()
 /**
  * Module dependencies.
  */
